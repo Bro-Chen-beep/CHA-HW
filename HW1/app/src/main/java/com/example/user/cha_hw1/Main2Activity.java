009a8a7d073adff4fdac651190568bcc7d10e0b8
EE91941387EE <37057027+EE91941387EE@users.noreply.github.com>
2018-04-18 00:20:52 +0800

    Little adjust

---
+++ HW1/app/src/main/java/com/example/user/cha_hw1/Main2Activity.java
@@ -0,0 +1,28 @@
+package com.example.user.cha_hw1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView mDisplayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+
+            String textEntered = "B10509005:"+intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+
+            mDisplayText.setText(textEntered);
+        }
+    }
+}
---
+++ HW1/app/src/main/java/com/example/user/cha_hw1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.user.cha_hw1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText inputtext;
+    private Button press;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        press = (Button) findViewById(R.id.press);
+        inputtext = (EditText) findViewById(R.id.inputtext);
+        press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textEntered = inputtext.getText().toString();
+                Context context = MainActivity.this;
+                Class destinationActivity = Main2Activity.class;
+                Intent startChildActivityIntent = new Intent(context, destinationActivity);
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
+                startActivity(startChildActivityIntent);
+            }
+        });
+    }
+}
